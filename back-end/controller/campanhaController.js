1ca54aab5b7d9160fff862112be06033a48eae49
Izabella-Nascimento <izabellanascimento1606@gmail.com>
2023-12-10 15:34:15 -0300

    Added campanha'routes

--- back-end/controller/campanhaController.js
+++ back-end/controller/campanhaController.js
@@ -1,4 +1,5 @@
 const pool = require("../database/index.js");
+const queryFormatter = require("node-pg-format");
 
 exports.cadastroCampanhaDeVacinacao = async (req, res) => {
   try {
@@ -6,7 +7,9 @@ exports.cadastroCampanhaDeVacinacao = async (req, res) => {
     const resultId = await pool.query(
       "select max(ID_CAMPANHA) + 1 as nextId from CAMPANHA"
     );
-    const nextId = resultId.rows[0]["nextId"];
+
+    const nextId = resultId.rows[0]["nextid"];
+
     const result = await pool.query(
       "insert into CAMPANHA(ID_CAMPANHA, DESCRICAO, DATA_INICIO, DATA_FIM) values ($1, $2, $3, $4) RETURNING *",
       [nextId, descricao, dataInicio, dataFim]
@@ -22,78 +25,89 @@ exports.cadastroCampanhaDeVacinacao = async (req, res) => {
 
 exports.edicaoCampanhaDeVacinacao = async (req, res) => {
   try {
-    const {idCampanha} = req.params;
-    //const { descricao, dataInicio, dataFim }
-    const result = await pool.query(
+    const { idCampanha } = req.params;
 
-    );
-    return res.json(result.rows[0]);  
+    const updatedValues = Object.values(req.body);
+    const updatedColumns = Object.keys(req.body).join(", ");
+
+    const queryString = `update CAMPANHA set(${updatedColumns}) = row(%L) where ID_CAMPANHA = %L RETURNING *`;
+    const formattedQuery = queryFormatter.formatWithArray(queryString, [
+      updatedValues,
+      idCampanha,
+    ]);
+
+    const result = await pool.query(formattedQuery);
+
+    return res.json(result.rows[0]);
   } catch (error) {
     console.log(error.message);
     return res.json({
-      message: "There was an error while editing the vaccination's campaign"
+      message: "There was an error while editing the vaccination's campaign",
     });
   }
 };
 
 exports.cadastroDeVacinaEmUmaCampanha = async (req, res) => {
- try {
-  const { idVacina } = req.params;
-  const {  } = req.body;
-  const result = await pool.query(
-    "insert into CAMPANHAVACINA(ID_VACINA) values ($1) RETURNING *",
-    [idVacina]
-  );
-
- } catch (error) {
-  console.log(error.message);
-  return res.json({
-    message: "There was an error while adding a vaccine in a campaign"
-  });
- }
+  try {
+    const { idVacina, idCampanha } = req.body;
+    const result = await pool.query(
+      "insert into CAMPANHAVACINA(ID_VACINA, ID_CAMPANHA) values ($1, $2) RETURNING *",
+      [idVacina, idCampanha]
+    );
+    return res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    return res.json({
+      message: "There was an error while adding a vaccine in a campaign",
+    });
+  }
 };
 
 exports.deletarVacinaDeUmaCampanha = async (req, res) => {
   try {
-    const { } = req.params;
-    await pool.query(
-      "delete from  where ",
-      []
+    const { idCampanha, idVacina } = req.params;
+    const result = await pool.query(
+      "delete from CAMPANHAVACINA where ID_CAMPANHA = $1 AND ID_VACINA = $2",
+      [idCampanha, idVacina]
     );
     return res.json();
   } catch (error) {
     console.error(error.message);
     return res.json({
-      message: "An error occurred when trying to exclude a vaccine from a campaign",
+      message:
+        "An error occurred when trying to exclude a vaccine from a campaign",
     });
   }
 };
 
 exports.consultaCampanhaPorData = async (req, res) => {
   try {
-    const {  } = req.params;
+    const { dataBuscada } = req.body;
     const result = await pool.query(
-      "select * from  where $1",
-      []
+      "select * from CAMPANHA where DATA_INICIO <= $1 AND DATA_FIM >= $1",
+      [dataBuscada]
     );
-    return res.json(result.rows[0]);
+    return res.json(result.rows);
   } catch (error) {
     console.error(error.message);
     return res.json({
-      message:
-        "An error occurred while searching for campaigns by date",
+      message: "An error occurred while searching for campaigns by date",
     });
   }
 };
 
 exports.consultaCampanhaProtecaoDaVacina = async (req, res) => {
   try {
-    const {  } = req.params;
+    const { doencaProtecao } = req.params;
     const result = await pool.query(
-      "select * from  where $1",
-      []
+      `select * from CAMPANHAVACINA inner join CAMPANHA 
+      on CAMPANHA.ID_CAMPANHA = CAMPANHAVACINA.ID_CAMPANHA 
+
+      inner join VACINA on VACINA.ID_VACINA = CAMPANHAVACINA.ID_VACINA
+      where VACINA.DOENCA_PROTECAO ILIKE $1`,
+      ["%" + doencaProtecao + "%"]
     );
-    return res.json(result.rows[0]);
+    return res.json(result.rows);
   } catch (error) {
     console.error(error.message);
     return res.json({
--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const pacienteController = require("./controller/pacienteController");
 const vacinaAplicadaController = require("./controller/vacinaAplicadaController");
+const campanhaController = require("./controller/campanhaController");
+const vacinaCosultaAnoController = require("./controller/vacinaCosultaAno");
+const vacinaController = require("./controller/vacinaController");
+const vacinaPacienteController = require("./controller/vacinaPacienteController");
+const vacinaCosultames = require("./controller/vacinaCosultames");
 
 const app = express();
 
@@ -28,16 +33,38 @@ app.delete(
   vacinaAplicadaController.excluirVacinaPaciente
 );
 
-app.get("/vacinaCosultaAno", vacinaCosultaAno.consultaAno);
-app.get("/vacinaCosultaAno", vacinaCosultaAno.consultaAnoFinal);
+app.get("/vacinaCosultaAno", vacinaCosultaAnoController.consultaAno);
+app.get("/vacinaCosultaAno", vacinaCosultaAnoController.consultaAnoFinal);
 
-app.get("/vacinaCosultames", vacinaCosultames.consultames);
-app.get("/vacinaCosultames", vacinaCosultames.consultamesFinal);
+//app.get("/vacinaCosultames", vacinaCosultames.consultames);
+//app.get("/vacinaCosultames", vacinaCosultames.consultamesFinal);
 
 app.get("/vacina/:doencaProtecao", vacinaController.consultaVacinaProtecao);
 
-app.get("/ConsultaVacinaPaciente/:id", vacinaPacienteController.consultaVacinaPaciente);
-app.get("/ConsultaVacinaPendente/:id", vacinaPacienteController.consultaVacinaPendente);
+app.get(
+  "/ConsultaVacinaPaciente/:id",
+  vacinaPacienteController.consultaVacinaPaciente
+);
+app.get(
+  "/ConsultaVacinaPendente/:id",
+  vacinaPacienteController.consultaVacinaPendente
+);
+
+app.post("/campanha", campanhaController.cadastroCampanhaDeVacinacao);
+app.patch(
+  "/campanha/:idCampanha",
+  campanhaController.edicaoCampanhaDeVacinacao
+);
+app.post("/campanha/vacina", campanhaController.cadastroDeVacinaEmUmaCampanha);
+app.delete(
+  "/campanha/:idCampanha/:idVacina",
+  campanhaController.deletarVacinaDeUmaCampanha
+);
+app.post("/campanha/data", campanhaController.consultaCampanhaPorData);
+app.get(
+  "/campanha/:doencaProtecao",
+  campanhaController.consultaCampanhaProtecaoDaVacina
+);
 
 app.listen(3000, () => {
   console.log("> Server is running");
