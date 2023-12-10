b308f2345b3d2eadbc43518a235bc34a3e8dc939
Izabella-Nascimento <izabellanascimento1606@gmail.com>
2023-12-10 02:08:52 -0300

    Created vaccine's route and changed error message

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pacienteController = require("./controller/pacienteController");
 const vacinaAplicadaController = require("./controller/vacinaAplicadaController");
+const vacinaController = require("./controller/vacinaController");
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.post("/paciente", pacienteController.cadastroPaciente);
 app.get("/vacinaAplicada/:idPaciente", vacinaAplicadaController.consultaVacinaPaciente);
 app.post("/vacinaAplicada/:idPaciente", vacinaAplicadaController.cadastroVacinaPaciente);
 app.delete("/vacinaAplicada/:idPaciente/:idVacina", vacinaAplicadaController.excluirVacinaPaciente);
+
+app.get("/vacina/:doencaProtecao", vacinaController.consultaVacinaProtecao);
 
 app.listen(3000, () => {
   console.log("server rodando");
